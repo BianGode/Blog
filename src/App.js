876a0097c6cd739ccdedd049d3646ede01945916
Bian <bian2205@gmail.com>
2022-11-15 09:34:18 +0100

    test

--- src/App.js
+++ src/App.js
@@ -61,7 +61,7 @@ function App() {
 
 	return (
 		<Routes>
-			<Route path='/' element={<>
+			<Route exact path='/' element={<>
 				<Header user={authentication} setAuth={setAuthentication} />
 				<Home />
 			</>
